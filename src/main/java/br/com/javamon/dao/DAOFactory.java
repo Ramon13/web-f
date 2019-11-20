4dfbf96cfd229226a5761b0ffd794ec68b103b3a
Ramon Santos <ramon.santoscosta@yahoo.com>
2019-11-20 15:08:36 -0300

    add a generic method to instantiate DAO classes

--- src/main/java/br/com/javamon/dao/DAOFactory.java
+++ src/main/java/br/com/javamon/dao/DAOFactory.java
@@ -1,7 +1,9 @@
 package br.com.javamon.dao;
 
+import br.com.javamon.exception.DAOException;
+
 /**
- * Implements the factory design pattern to provides differents DAO's instences
+ * Implements the factory design pattern to provides different DAO's instences
  * @author Ramon (based on Carlos Tosin tutorial code. Softblue course)
  * @version 1.0
  *
@@ -21,6 +23,20 @@ public class DAOFactory {
 		if(instance == null)
 			instance = new DAOFactory();
 		return instance;
+	}
+	
+	/**
+	 * Generic class to instantiate DAO's objects
+	 * @param clazz - the class object
+	 * @return the DAO object instantiated through reflection api
+	 * @throws DAOException wrap thrown exceptions from reflection instantiation
+	 */
+	public <T extends DAO<?>> T getDAO(Class<T> clazz) throws DAOException{
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new DAOException(e);
+		}
 	}
 	
 }

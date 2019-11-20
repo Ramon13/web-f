ae87b89d761160b020857dbcd3bf54dec936fb94
Ramon Santos <ramon.santoscosta@yahoo.com>
2019-11-20 17:41:43 -0300

    implemented the first version of controller layer

---
+++ src/main/java/br/com/javamon/action/Action.java
@@ -0,0 +1,74 @@
+package br.com.javamon.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.javamon.util.HibernateUtil;
+
+/**
+ * This super class provides common methods to deal with http request and responses operations
+ * Implements the design pattern "template method", automating Hibernate transactional process (#runAction)
+ * @author Ramon (based on Carlos Tosin tutorial code. Softblue course.)
+ * @version 1.0
+ */
+public abstract class Action {
+
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	/**
+	 * Abstract method. This method should be implemented by subclasses 
+	 * @throws Exception 
+	 */
+	public abstract void process() throws Exception;
+	
+	/**
+	 * Template method design pattern
+	 * @throws Exception
+	 */
+	public void runAction() throws Exception{
+		try{
+			HibernateUtil.beginTransaction();
+			process();
+			HibernateUtil.commitTransaction();
+		}catch(Exception e){
+			HibernateUtil.rollbackTransaction();
+			throw e;
+		}
+	}
+	
+	/**
+	 * Fowards a request
+	 * @param path for the request to be sent
+	 * @throws Exception
+	 */
+	protected void foward(String path) throws Exception{
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+	
+	/**
+	 * Redirects a request
+	 * @param path for the request to be sent
+	 * @throws Exception
+	 */
+	protected void redirect(String path) throws Exception{
+		String contextRoot = request.getContextPath();
+		response.sendRedirect(contextRoot + "/" + path);
+	}
+	
+	public void setRequest(HttpServletRequest request){
+		this.request = request;
+	}
+	
+	protected HttpServletRequest getRequest(){
+		return request;
+	}
+	
+	public void setResponse(HttpServletResponse response){
+		this.response = response;
+	}
+	
+	protected HttpServletResponse getResponse(){
+		return response;
+	}
+}
---
+++ src/main/java/br/com/javamon/servlet/ControllerServlet.java
@@ -0,0 +1,77 @@
+package br.com.javamon.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.javamon.action.Action;
+
+/**
+ * This class handles with all request and send to specifics actions
+ * @author Ramon (based on Carlos Tosin tutorial code. Softblue course.)
+ * @version 1.0
+ */
+public class ControllerServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Actions file. This file maps actions to classes
+	 */
+	private static Properties actions;
+	
+	/**
+	 * This static block inits and load the actions object only one time in application
+	 */
+	static{
+		try(InputStream inStream = ControllerServlet.class.getResourceAsStream("/action.properties")){
+			actions = new Properties();
+			actions.load(inStream);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+	
+	/**
+	 * Handles with get and post requests.
+	 * Identify and submits the request to specific action
+	 * @param request the request object provides by container
+	 * @param response the response object provides by container
+	 * @throws ServletException wrap all lower exceptions and throw then as ServletException
+	 */
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+		String path = request.getServletPath();
+		//get all after / and before .action
+		path = path.substring(1, path.indexOf('.'));
+		
+		String actionClass = actions.getProperty(path);
+		
+		if(actionClass == null)
+			throw new ServletException("The action '" + path + "' cannot be mapped.");
+		
+		try{
+			Action action = (Action) Class.forName(actionClass).newInstance();
+			action.setRequest(request);
+			action.setResponse(response);
+			action.runAction();
+			
+		}catch(Exception e){
+			throw new ServletException(e);
+		}
+	}
+}

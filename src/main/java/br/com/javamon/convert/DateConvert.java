aefc54a77cecab35567f1f53df5f9becbc81a784
Ramon Santos <ramon.santoscosta@yahoo.com>
2019-11-27 16:28:02 -0300

    Create DateConvert class. This class provides common methods to handle with date conversions.

---
+++ src/main/java/br/com/javamon/convert/DateConvert.java
@@ -0,0 +1,43 @@
+package br.com.javamon.convert;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Locale;
+
+import br.com.javamon.exception.ConvertException;
+
+/**
+ * This class provides common statics methods to handles with date conversions tasks
+ * @author ramon
+ * @version 1.0
+ *
+ */
+public class DateConvert extends Convert {
+
+	/**
+	 * Convert java.time.LocalDate to java.util.Date
+	 * @param localDate the object to be converted
+	 * @return the converted java.util.Date
+	 * @throws ConvertException
+	 */
+	public static Date localDateToDate(LocalDate localDate) throws ConvertException{
+		return Date.from(localDate.atStartOfDay()
+				.atZone(ZoneId.systemDefault())
+				.toInstant());
+	}
+	
+	/**
+	 * Parse java.time.LocalDate to java.util.String following a specific pattern
+	 * @param localdate the object to be parsed
+	 * @param datePattern the date pattern to be applied
+	 * @param locale the Locale object to be used with datePattern
+	 * @return the date formated according locale and pattern
+	 * @throws ConvertException
+	 */
+	public static String parseLocalDateToString(LocalDate localdate, String datePattern, Locale locale) throws ConvertException{
+		SimpleDateFormat sdf = new SimpleDateFormat(datePattern, locale);
+		return sdf.format(Date.from(localdate.atStartOfDay(ZoneId.systemDefault()).toInstant() ) );
+	}
+}
